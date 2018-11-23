ea15f38a87ca7b344ccbd40df27cbdf51b946676
Chris DuPuis <cpdupuis@gmail.com>
2018-11-22 22:48:25 -0800

    Maybe I'll use Lisp for my language.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"os"
 	"fmt"
 	"bufio"
@@ -17,29 +18,40 @@ type Interpreter struct {
 	Reader *bufio.Reader
 }
 
+
 type CommandStatus int
 const (
-	Complete = iota
-	Incomplete
+	Continue = iota
 	Quit
 )
+
+
+type handlerFunc func([]string) (Value,error)
+
+var handlerTable = map[string]handlerFunc{}
+
+func def(words []string) (Value,error) {
+	return "",nil	
+}
+
+func init() {
+	handlerTable["def"] = def
+}
 
 // Return the next complete parseable chunk.
 func read(interp *Interpreter) (string, CommandStatus) {
 	fmt.Printf("$ ")
 	val, _ := interp.Reader.ReadString('\n')
 	chunk := strings.TrimRight(val, "\r\n")
-	return chunk,Complete
+	return chunk,Continue
 }
 
 func eval(interp *Interpreter, cmd string) (Value, CommandStatus) {
-	// For now, let's just return "woo hoo" if the value is "hello world", and otherwise "yup"
-	if cmd == "hello world" {
-		return "woo hoo", Complete
-	} else if cmd == "exit" {
-		return "", Quit
+	words := strings.Split(cmd, " ")
+	if len(words) == 0 {
+		return "", Continue
 	} else {
-		return fmt.Sprintf("yup: <%s>", cmd), Complete
+		return fmt.Sprintf("Yeah. %+v", words), Continue
 	}
 }
 
@@ -47,24 +59,69 @@ func print(interp *Interpreter, value Value) {
 	fmt.Printf("%+v\n", value)
 }
 
-func repl(interp *Interpreter) {
+func repl(tokenCh chan string) {
 	for {
-		cmd, stat := read(interp)
-		if stat == Quit {
-			return
-		}
-		v, stat := eval(interp, cmd)
-		if stat == Quit {
-			return
-		}
-		print(interp, v)
+		token := <- tokenCh
+		fmt.Printf("Token: <%v>\n", token)
 	}
 }
 
 
-func main() {
-	interp := &Interpreter {
-		Reader: bufio.NewReader(os.Stdin),
+func tokenize(rawReader io.Reader, outCh chan string) error {
+	bufReader := bufio.NewReader(rawReader)
+	currTok := make([]byte, 0)
+	tokOut := func() {
+		trimTok := strings.Trim(string(currTok), " ")
+		if len(trimTok) > 0 {
+			outCh <- trimTok
+		}
+		currTok = make([]byte, 0)
 	}
-	repl(interp)
+	inStr := false
+	for {
+		b, err := bufReader.ReadByte()
+		if err != nil {
+			return err
+		}
+		switch b {
+		case '"':
+			if inStr {
+				// We just ended a string, and therefore a token
+				currTok = append(currTok, b)
+				tokOut()
+				inStr = false
+			} else {
+				// We're just entering a string
+				tokOut()
+				currTok = make([]byte,1)
+				currTok[0] = '"'
+				inStr = true
+			}
+		case ' ':
+			if inStr {
+				currTok = append(currTok, b)
+			} else {
+				tokOut()
+			}
+		case '\r':
+			// We don't talk about this character.
+		case '\n':
+			tokOut()
+		case '(':
+			tokOut()
+			outCh <- "("
+		case ')':
+			tokOut()
+			outCh <- ")"
+		default:
+			currTok = append(currTok, b)
+		}
+
+	}
+}
+
+func main() {
+	tokenCh := make(chan string, 256)
+	go tokenize(os.Stdin, tokenCh)
+	repl(tokenCh)
 }
