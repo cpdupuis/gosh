0d997eb2ff3c3af8b79d771d74559f92ab9aee17
Chris DuPuis <cpdupuis@gmail.com>
2018-11-25 21:28:26 -0800

    Move interpreter to package interp.

---
+++ interp/interp.go
@@ -0,0 +1,37 @@
+package interp
+
+import (
+	"os"
+	"fmt"
+	"github.com/cpdupuis/gosh/lang"
+)
+
+func repl(treeCh chan lang.Value) {
+	env := lang.NewEnvironment()
+	for {
+		tree := <-treeCh
+		fmt.Printf("Tree: <%+v>\n", tree)
+		res,err := tree.Eval(env.Root)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err.Error())
+		} else {
+			fmt.Printf("Result: %+v\n", res)
+		}
+	}
+}
+
+
+func treeize(inCh chan string, outCh chan lang.Value) lang.Value {
+	for {
+		sexp,_ := lang.ParseSExp(inCh)
+		outCh <- sexp
+	}
+}
+
+func Interp() {
+	tokenCh := make(chan string, 256)
+	treeCh := make(chan lang.Value, 256)
+	go lang.Tokenize(os.Stdin, tokenCh)
+	go treeize(tokenCh, treeCh)
+	repl(treeCh)
+}
--- main.go
+++ main.go
@@ -1,37 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"github.com/cpdupuis/gosh/lang"
+	"github.com/cpdupuis/gosh/interp"
 )
 
-func repl(treeCh chan lang.Value) {
-	env := lang.NewEnvironment()
-	for {
-		tree := <-treeCh
-		fmt.Printf("Tree: <%+v>\n", tree)
-		res,err := tree.Eval(env.Root)
-		if err != nil {
-			fmt.Printf("Error: %v\n", err.Error())
-		} else {
-			fmt.Printf("Result: %+v\n", res)
-		}
-	}
-}
-
-
-func treeize(inCh chan string, outCh chan lang.Value) lang.Value {
-	for {
-		sexp,_ := lang.ParseSExp(inCh)
-		outCh <- sexp
-	}
-}
-
 func main() {
-	tokenCh := make(chan string, 256)
-	treeCh := make(chan lang.Value, 256)
-	go lang.Tokenize(os.Stdin, tokenCh)
-	go treeize(tokenCh, treeCh)
-	repl(treeCh)
+	interp.Interp()
 }
