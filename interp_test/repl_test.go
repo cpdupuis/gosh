5bc7a582e83b11761530ee3d8305e8ce2c634ef4
Chris DuPuis <cpdupuis@gmail.com>
2018-12-03 19:41:42 -0800

    Finished TestInterpDefQuoteEval

--- interp_test/repl_test.go
+++ interp_test/repl_test.go
@@ -29,6 +29,9 @@ func TestInterpDefQuoteEval(t *testing.T) {
 	cmd1 := &lang.Cons{First:&lang.Int{Number:8675309}, Rest:lang.Nil}
 	cmd1 = &lang.Cons{First:&lang.Symbol{Sym:"a"}, Rest:cmd1}
 	cmd1 = &lang.Cons{First:&lang.Symbol{Sym:"def"}, Rest:cmd1}
+	if cmd1.String() != "( def a 8675309 )" {
+		t.Fail()
+	}
 	env := lang.NewEnvironment()
 	res,err := interp.ReplOne(cmd1, env)
 	if err != nil {
@@ -41,6 +44,36 @@ func TestInterpDefQuoteEval(t *testing.T) {
 	} else {
 		t.Fail()
 	}
-//	cmd2 := &lang.Cons{First:&lang.Symbol{Sym:"a"}, Rest:lang.Nil}
-
+	cmd2 := &lang.Cons{First:&lang.Symbol{Sym:"a"}, Rest:lang.Nil}
+	cmd2 = &lang.Cons{First:&lang.Symbol{Sym:"quote"}, Rest: cmd2}
+	cmd2 = &lang.Cons{First:cmd2, Rest:lang.Nil}
+	cmd2 = &lang.Cons{First:&lang.Symbol{Sym:"b"}, Rest: cmd2}
+	cmd2 = &lang.Cons{First: &lang.Symbol{Sym:"def"}, Rest: cmd2}
+	if cmd2.String() != "( def b ( quote a ) )" {
+		t.Fail()
+	}
+	res,err = interp.ReplOne(cmd2, env)
+	if err != nil {
+		t.Fail()
+	}
+	if resym,ok := res.(*lang.Symbol); !ok {
+		t.Fail()
+	} else {
+		if resym.Sym != "a" {
+			t.Fail()
+		}
+	}
+	cmd3 := &lang.Cons{First:&lang.Symbol{Sym:"b"}, Rest:lang.Nil}
+	cmd3 = &lang.Cons{First:&lang.Symbol{Sym:"eval"}, Rest:cmd3}
+	val,err := interp.ReplOne(cmd3, env)
+	if err != nil {
+		t.Fail()
+	}
+	if anInt,ok := val.(*lang.Int); !ok {
+		t.Fail()
+	} else {
+		if anInt.Number != 8675309 {
+			t.Fail()
+		}
+	}
 }
